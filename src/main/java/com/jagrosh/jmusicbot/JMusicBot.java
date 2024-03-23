b9c0dca4c92edbb58e93a70c1064f6c20caa5f34
Jadua1010 <jmb-n@outlook.com>
2024-03-23 21:05:50 +0100

    Small update
    Increased LavaPlayer version
    Changed errors to info messages

--- src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
+++ src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
@@ -126,7 +126,7 @@ public class JMusicBot
             String unsupportedReason = OtherUtil.getUnsupportedBotReason(jda);
             if (unsupportedReason != null)
             {
-                prompt.alert(Prompt.Level.ERROR, "JMusicBot", "JMusicBot cannot be run on this Discord bot: " + unsupportedReason);
+                prompt.alert(Prompt.Level.INFO, "JMusicBot", "JMusicBot cannot be run on this Discord bot: " + unsupportedReason);
                 try{ Thread.sleep(5000);}catch(InterruptedException ignored){} // this is awful but until we have a better way...
                 jda.shutdown();
                 System.exit(1);
