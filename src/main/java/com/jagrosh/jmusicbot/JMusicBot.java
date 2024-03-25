22d31d7ca7463e1649c721a00daf79afb5456122
Jadua1010 <jmb-n@outlook.com>
2024-03-25 01:39:23 +0100

    Revert "Remove delay to re-enable concurrency"
    This reverts commit cf4e9c709b55732e37bd8c1f37b3c9bd4f6e9784.

--- src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
+++ src/main/java/com/jagrosh/jmusicbot/JMusicBot.java
@@ -66,10 +66,13 @@ public class JMusicBot
                         // Call startBot() shardCount times
                         for (int i = 0; i < shardCount; i++) {
                             startBot(i,shardCount);
+                            Thread.sleep(5000);
                         }
                     } catch (NumberFormatException e) {
                         // Handle invalid shard count format
                         System.err.println("Invalid shard count format");
+                    } catch (InterruptedException ex) {
+                        java.util.logging.Logger.getLogger(JMusicBot.class.getName()).log(Level.SEVERE, null, ex);
                     }
                 }
             } else if (firstArg.equals("generate-config")) {
